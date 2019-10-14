eb7f8ab2dd33b2206c41cc95e041218de57d3d80
JaredLopezAlamilla <56237409+JaredLopezAlamilla@users.noreply.github.com>
2019-10-14 15:18:09 +1300

    Create HybridPDF-PNG2D.m

---
+++ HybridPDF-PNG2D.m
@@ -0,0 +1,10 @@
+% This scripts cotains intructions to produce and
+% hybrid PDF-PNG 2D image within matlab
+
+% When a density plot or contour plot in matlab
+% is generated from big array the eps/pdf tools 
+% for exportation produce artifacts in the image that are unpleasant
+% Also the size of the produced vectorail imaga can exceed the 20Mb
+
+% For this purpose a hybrid file is better for publication
+
