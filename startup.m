12d0b52f9753885508753cbf1991b908379a4d24
JaredLopezAlamilla <56237409+JaredLopezAlamilla@users.noreply.github.com>
2019-10-15 14:43:28 +1300

    Update startup.m

--- startup.m
+++ startup.m
@@ -3,13 +3,13 @@
 % Default interpreter set to LaTex
 % Default font size
 % Default colormaps
-%set(0,'DefaultTextInterpreter','Latex');
+set(0,'DefaultTextInterpreter','Latex');
 set(groot, 'defaultAxesTickLabelInterpreter','latex');
 set(0,'DefaultAxesFontSize', 24,'DefaultAxesLineWidth',2.5,'DefaultTextInterpreter','Latex');
 set(groot, 'defaultAxesTickLabelInterpreter','latex');
 
 % select between the different colormaps best suit your puropose 
-% uncomment line 14 to make default colormap: matlabs' parula 
+% uncomment line 16 to make default colormap: matlabs' parula 
 % uncomment lines 18-19 to make default colormap: a nice gray scale
 % uncomment line 21-22 to make  default colormap: wolfram-mathematica darkrainbow palette
 
