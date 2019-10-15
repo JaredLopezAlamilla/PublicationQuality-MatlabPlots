bd4a173465b65364f2659f4c36a136bd7b363009
JaredLopezAlamilla <56237409+JaredLopezAlamilla@users.noreply.github.com>
2019-10-15 15:02:24 +1300

    Update UniformFigs3D.m

--- UniformFigs3D.m
+++ UniformFigs3D.m
@@ -1,17 +1,19 @@
 function [fig1,ax1] =UniformFigs3D(num)
-
+% This function creates uniform size figures created by: mesh, surf, plot3
+% the final size is perfect for 2column journals, e.g., APSjournals: PRE, PRL,
+% >> [fig1,ax1]=UniformFigs3D(1); % will resize figure number 1 and save handles
     fig1=figure(num);
     ax1=gca;
     set(ax1,'ActivePositionProperty','Position');
 
-    %------------ Posicion y Tamaños de la ventana ---------------------------
-    %-- set(fig1,'Position', [Xo Yo Ancho Alto]);
+    %------------ Position y window size ---------------------------
+    %-- set(fig1,'Position', [Xo Yo width height]);
     set(fig1,'Units','points');
     set(fig1, 'OuterPosition', [10 30 581 546.5]);
     %Final size after export is
-    %    {Ancho-12, Alto-69}
-    %------------ Posicion y Tamaños de los ejes   ---------------------------
-    %-- set(ax1,'Position', [Xo Yo Ancho Alto]);
+    %    {width-12pt, height-69pt}
+    %------------ Axis position and size    ---------------------------
+    %-- set(ax1,'Position', [Xo Yo width height]);
     set(ax1,'Units','points');
     set(ax1,'Position', [98 88 390 347]);
 %     set(ax1,'Position', [98 84 390 347]);
