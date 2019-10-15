4143d744f71d21d6c566073ccbb2e2a117c0fb72
JaredLopezAlamilla <56237409+JaredLopezAlamilla@users.noreply.github.com>
2019-10-15 15:03:25 +1300

    Update UniformFigs3D.m

--- UniformFigs3D.m
+++ UniformFigs3D.m
@@ -6,13 +6,13 @@ function [fig1,ax1] =UniformFigs3D(num)
     ax1=gca;
     set(ax1,'ActivePositionProperty','Position');
 
-    %------------ Position y window size ---------------------------
+    %------------ Position relative to screen & window size ---------------------------
     %-- set(fig1,'Position', [Xo Yo width height]);
     set(fig1,'Units','points');
     set(fig1, 'OuterPosition', [10 30 581 546.5]);
     %Final size after export is
     %    {width-12pt, height-69pt}
-    %------------ Axis position and size    ---------------------------
+    %------------ Axis position & size    ---------------------------
     %-- set(ax1,'Position', [Xo Yo width height]);
     set(ax1,'Units','points');
     set(ax1,'Position', [98 88 390 347]);
